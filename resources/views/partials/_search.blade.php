1320dcd8b37598c6a3c8b638ae3d77703af2a143
panos42 <119461079+panos42@users.noreply.github.com>
2023-10-03 00:05:56 +0300

    small changes

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -110,17 +110,17 @@
 
 .menu-item {
     display: inline-block;
-    padding: 10px 20px; /* Adjust padding as needed */
+    padding: 10px 15px; /* Adjust padding as needed */
     font-size: 16px; /* Adjust font size as needed */
     cursor: pointer;
-    background-color: #007bff;
+    background-color: #204368;
     color: #fff;
     border-radius: 25px; /* Make the items round */
-    margin-right: 10px; /* Adjust spacing between items */
+    margin-right: 3px; /* Adjust spacing between items */
 }
 
 .menu-item:hover {
-    background-color: #0056b3; /* Change color on hover */
+    background-color: rgb(30, 97, 168); /* Change color on hover */
 }
 
     
@@ -128,7 +128,7 @@
         /* Media query for smaller screens */
         @media only screen and (max-width: 768px) {
             .horizontal-scroll-menu{
-                width: 300px ;
+                width: 350px ;
             }
 
            
@@ -176,14 +176,14 @@
     <div class="invisible-box">
 
 
-        <form action="/" class="search-container">
+        <form action="/" class="search-container" id="searchForm">
             <div class="main-search">
                 <div class="group">
 
                 <svg class="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
                 <input
                     placeholder="Search for jobs..."
-                    {{-- id="searchInput" --}}
+                    id="searchInput"
                     class="searchInput"
                     name="search"
                     type="search"
@@ -239,18 +239,27 @@
             </div>
             <div class="horizontal-scroll-menu">
                 <ul class="menu-list">
-                    <li class="menu-item">AI/ML</li>
-                    <li class="menu-item">Cybersecurity</li>
-                    <li class="menu-item">Education</li>
-                    <li class="menu-item">Marketing</li>
-                    <li class="menu-item">Engineer</li>
-                    <li class="menu-item">Tutor</li>
-                    <li class="menu-item">Sales</li>
-                    <li class="menu-item">Freelance</li>
-                    <li class="menu-item">Backend</li>
-                    <li class="menu-item">Education</li>
-                    <li class="menu-item">Education</li>
+                    <li class="menu-item" onclick="selectMenuItem('AI')">AI/ML</li>
+<!-- Add similar onclick attributes to other menu items -->
+<li class="menu-item" onclick="selectMenuItem('Cybersecurity')">Cybersecurity</li>
+<li class="menu-item" onclick="selectMenuItem('Education')">Education</li>
+<li class="menu-item" onclick="selectMenuItem('Marketing')">Marketing</li>
+<li class="menu-item" onclick="selectMenuItem('Engineer')">Engineer</li>
+<li class="menu-item" onclick="selectMenuItem('Tutor')">Tutor</li>
+<li class="menu-item" onclick="selectMenuItem('Sales')">Sales</li>
 
+<li class="menu-item" onclick="selectMenuItem('Freelance')">Freelance</li>
+<li class="menu-item" onclick="selectMenuItem('Backend')">Backend</li>
+<li class="menu-item" onclick="selectMenuItem('Sales')">Sales</li>
+
+<li class="menu-item" onclick="selectMenuItem('Teacher')">Teacher</li>
+<li class="menu-item" onclick="selectMenuItem('Sales')">Sales</li>
+<li class="menu-item" onclick="selectMenuItem('Media')">Media</li>
+<li class="menu-item" onclick="selectMenuItem('Writer')">Writer</li>
+<li class="menu-item" onclick="selectMenuItem('Blockchain')">Blockchain</li>
+<li class="menu-item" onclick="selectMenuItem('SEO')">SEO</li>
+
+              
                     <!-- Add more list items for additional industries -->
                 </ul>
             </div>
@@ -262,19 +271,17 @@
 
    
     <script>
-        // JavaScript example to dynamically populate menu items
-document.addEventListener("DOMContentLoaded", function() {
-    const menuList = document.querySelector(".menu-list");
-    const industries = ["Industry 1", "Industry 2", "Industry 3", /* Add more industries */];
-
-    industries.forEach(industry => {
-        const listItem = document.createElement("li");
-        listItem.className = "menu-item";
-        listItem.textContent = industry;
-        menuList.appendChild(listItem);
-    });
-});
-
-    </script>
+        function selectMenuItem(text) {
+            // Get the search input element by its id
+            var searchInput = document.getElementById('searchInput');
+            
+            // Set the value of the search input to the selected menu item's text
+            searchInput.value = text;
+            
+            // Trigger the form submission (assuming you have a form with the id 'searchForm')
+            document.getElementById('searchForm').submit();
+        }
+        </script>
+        
 </body>
 </html>
